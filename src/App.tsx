54ea5d90f8c942a0f6da5c07654910884f580697
elahe-habibi98 <elahe.hbb.77@gmail.com>
2024-04-06 11:25:14 +0330

    init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="border border-black text-red-500" >
+        Click on the Vite and React logos to learn more
+      </div>
+    </>
+  )
+}
+
+export default App
